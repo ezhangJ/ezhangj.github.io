33a9732b625e8a6967f9f6b679dd522bad1c09dc
Eric Zhang <ezhang56@gmail.com>
2018-07-08 01:33:53 -0700

    implemented scrolling

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
+
+
 $(document).ready(function(){
-    $("#triangleUp").click(function(){
-        $('html, body').animate({scrollTop: '0px'}, 1000);
+    $("#downscroll1").click(function(){
+        $('html, body').animate({scrollTop: $("#page1").offset().top + 10}, 1000);
     });
-    $("#triangleDown").click(function(){
-        $('html, body').animate({scrollTop: $(document).height()}, 1000);
-    })
-    $(document).on('click', '.navlink', function(event){
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 500);
+    $("#downscroll2").click(function(){
+        $('html, body').animate({scrollTop: $("#page3").offset().top}, 1000);
     });
 });
